670b6fec1722c48569ced2fbff9e511d374113d2
razyd <contact.bstack@gmail.com>
2025-07-15 18:49:53 +0200

    Active routers: /users y /movies at the same time

--- clase-5/app.js
+++ clase-5/app.js
@@ -11,20 +11,24 @@ app.use(corsMiddleware())
 
 app.disable('x-powered-by')
 
-const ROUTER_TO_USE = 'users' // Cambia a 'movies' o 'users' según sea necesario
+// const ROUTER_TO_USE = 'users' // Cambia a 'movies' o 'users' según sea necesario
 
-if (ROUTER_TO_USE === 'users') {
-  app.use('/users', usersRouter)
-} else if (ROUTER_TO_USE === 'movies') {
-  app.use('/movies', moviesRouter)
-} else {
-  console.warn(
-    `Router "${ROUTER_TO_USE}" no está configurado. No se montará ningún router.`
-  )
-}
+// if (ROUTER_TO_USE === 'users') {
+//   app.use('/users', usersRouter)
+// } else if (ROUTER_TO_USE === 'movies') {
+//   app.use('/movies', moviesRouter)
+// } else {
+//   console.warn(
+//     `Router "${ROUTER_TO_USE}" no está configurado. No se montará ningún router.`
+//   )
+// }
+
+app.use('/movies', moviesRouter)
+app.use('/users', usersRouter)
 
 const PORT = process.env.PORT ?? 1234
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
-  console.log(`Router activo: ${ROUTER_TO_USE}`)
+  // console.log(`Router activo: ${ROUTER_TO_USE}`) Por si quiero sólo activar uno
+  console.log('Routers activos: /users y /movies')
 })
