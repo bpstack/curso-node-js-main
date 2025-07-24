e9d7bf7cf9a5dcd26981f42d99614ba587ffae5b
razyd <contact.bstack@gmail.com>
2025-07-24 12:40:46 +0200

    Eliminación de JWT de api.http y buenas prácticas con .env

--- node-js-user-auth/config/config.js
+++ node-js-user-auth/config/config.js
@@ -1,18 +1,14 @@
+// Cargar variables de entorno desde .env si estás usando dotenv
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Verificación crítica: asegúrate de tener la clave secreta JWT
+if (!process.env.SECRET_JWT_KEY) {
+  throw new Error('Falta la variable SECRET_JWT_KEY en el entorno')
+}
+
 export const {
   PORT = 3000,
   SALT_ROUNDS = 10, // Número de rondas para hashear contraseñas, puedo cambiarlo dependiendo si estoy en produccion o desarrollo
-  SECRET_JWT_KEY = 'this-is-an-awesome-secret-key', // No se recomienda poner un valor por defecto, para que así seguro no se olvide en producción (porque nos dará error) e inyectarselo como variable de entorno. debería estar en un archivo .env
+  SECRET_JWT_KEY, // No se recomienda poner un valor, para que así seguro no se olvide en producción (porque nos dará error) e inyectarselo como variable de entorno. debería estar en un archivo .env
 } = process.env
-
-// El código va a quedar de la siguiente manera:
-
-// if (!process.env.JWT_SECRET_KEY) {
-//   throw new Error('Falta la variable JWT_SECRET en el entorno')
-// }
-
-// export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
-
-// export const {
-//   PORT = 3000,
-//   SALT_ROUNDS = 10
-// } = process.env
